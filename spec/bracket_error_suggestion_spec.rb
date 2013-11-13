5bf6ef88a1a03991ea50a85cd6358fc4088fae3f
akima <akm2000@gmail.com>
2013-11-14 02:28:22 +0900

    spec記述中

--- spec/bracket_error_suggestion_spec.rb
+++ spec/bracket_error_suggestion_spec.rb
@@ -5,7 +5,24 @@ describe BracketErrorSuggestion do
     BracketErrorSuggestion::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  let(:tree) do
+    {
+      foo1: {bar1: :baz1},
+      "foo2" => [{"bar1" => :baz1}, {"bar2" => :baz2}],
+    }
   end
+
+  context :disabled do
+    it{ tree[:foo1].should be_a(Hash) }
+    it{ tree[:foo1][:bar1].should == :baz1 }
+    it{ tree[:foo1][:bar2].should be_nil }
+    it{ tree["foo2"][99].should be_nil }
+    it "raises NoMethodError" do
+      expect{ tree[:foo1][:bar2][:baz2] }.to raise_error(NoMethodError, "undefined method `[]' for nil:NilClass")
+    end
+    it "raises TypeError" do
+      expect{ tree["foo2"][99] }.to raise_error(TypeError, "undefined method `[]' for nil:NilClass")
+    end
+  end
+
 end
