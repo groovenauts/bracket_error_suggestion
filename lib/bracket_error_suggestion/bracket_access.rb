f642ccf6488a021674e442e8ae69d8a005e6613c
akima <akm2000@gmail.com>
2013-11-14 16:56:38 +0900

    メソッド名を修正しました

--- lib/bracket_error_suggestion/bracket_access.rb
+++ lib/bracket_error_suggestion/bracket_access.rb
@@ -6,37 +6,37 @@ module BracketErrorSuggestion
     class << self
       def included(klass)
         klass.module_eval do
-          alias_method :get_without_trace, :[]
-          alias_method :[], :get_with_trace
+          alias_method :_get_without_be_suggestion, :[]
+          alias_method :[], :_get_with_be_suggestion
         end
       end
     end
 
-    attr_accessor :path
+    attr_accessor :path_on_be_suggestion
 
-    def get_with_trace(name)
+    def _get_with_be_suggestion(name)
       if BracketErrorSuggestion.enabled
         begin
-          r = get_without_trace(name)
+          r = _get_without_be_suggestion(name)
         rescue TypeError => e
-          s = path || "<#{self.class.name}>"
-          raise TypeError, "#{e.message}, it attempted to access #{s}[#{name.inspect}] but #{s} is an Array"
+          raise TypeError, "#{e.message}, it attempted to access #{_actual_path_on_be_suggestion}[#{name.inspect}] but #{_actual_path_on_be_suggestion} is an Array"
         end
         return r unless BracketErrorSuggestion.enabled
         case r
         when nil then
-          s = path || "<#{self.class.name}>"
-          BracketErrorSuggestion.nil_parent_paths << "#{s}[#{name.inspect}]"
+          BracketErrorSuggestion.nil_parent_paths << "#{_actual_path_on_be_suggestion}[#{name.inspect}]"
         when Array, Hash then
-          s = path || "<#{self.class.name}>"
-          r.path = "#{s}[#{name.inspect}]"
+          r.path_on_be_suggestion = "#{_actual_path_on_be_suggestion}[#{name.inspect}]"
         end
         return r
       else
-        return get_without_trace(name)
+        return _get_without_be_suggestion(name)
       end
     end
 
+    def _actual_path_on_be_suggestion
+      path_on_be_suggestion || "<#{self.class.name}>"
+    end
   end
 
 end
--- lib/bracket_error_suggestion/nil_ext.rb
+++ lib/bracket_error_suggestion/nil_ext.rb
@@ -6,21 +6,21 @@ module BracketErrorSuggestion
     class << self
       def extended(obj)
         obj.instance_eval do
-          alias :method_missing_without_bracket_error_suggestion :method_missing
-          alias :method_missing :method_missing_with_bracket_error_suggestion
+          alias :method_missing_without_be_suggestion :method_missing
+          alias :method_missing :method_missing_with_be_suggestion
         end
       end
     end
 
-    def method_missing_with_bracket_error_suggestion(name, *args, &block)
-      return method_missing_without_bracket_error_suggestion(name, *args, &block) unless BracketErrorSuggestion.enabled
+    def method_missing_with_be_suggestion(name, *args, &block)
+      return method_missing_without_be_suggestion(name, *args, &block) unless BracketErrorSuggestion.enabled
       case name
       when :[] then
         key = args.first
         raise NoMethodError, "undefined method `[]' for nil:NilClass, maybe it attempted to access: " +
           BracketErrorSuggestion.nil_parent_paths.reverse.map{|path| "#{path}[#{key.inspect}] but #{path} is nil"}.join(", ")
       end
-      return method_missing_without_bracket_error_suggestion(name, *args, &block)
+      return method_missing_without_be_suggestion(name, *args, &block)
     end
   end
 end
